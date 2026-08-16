7f58309556e164a8a053b4d57dacfe92688a2523
prigeosc <prigeosc@gmail.com>
2026-08-16 12:26:26 -0300

    Refactor Firebase configuration variable naming

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+/* =========================================================================
+   SINCRONIZAÇÃO ENTRE DISPOSITIVOS (Firebase)
+   -------------------------------------------------------------------------
+   Projeto: paineldospets
+   ========================================================================= */
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBilCAAiu3R-03euKZFUx2xMConZFG6eBI",
   authDomain: "paineldospets.firebaseapp.com",
   projectId: "paineldospets",
@@ -12,6 +12,3 @@ const firebaseConfig = {
   messagingSenderId: "995192692618",
   appId: "1:995192692618:web:15b56b65099b6264717273"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
